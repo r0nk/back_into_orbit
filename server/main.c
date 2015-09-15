29bc3b483d4941714ac26ba8bed6034a92722948
r0nk <r00nk@simplecpu.com>
2015-09-15 17:53:52 +0000

    multiple server connection accepts

--- server/main.c
+++ server/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <sys/socket.h>
 
-int main(){
+int init_server()
+{
 	int portnum = 2903;
 	struct sockaddr_in address;
 	int err = 0;
-	int sock = 0;// for each client
 	int server_socket = socket(PF_INET,SOCK_STREAM,0);
 
 	address.sin_family = AF_INET;
 	address.sin_port = htons(portnum);
 
 	printf("port number:%i\n",portnum);
-
 
 	err = bind(server_socket,(struct sockaddr *)&address,
 			sizeof(struct sockaddr_in));
@@ -25,7 +25,6 @@ int main(){
 		printf("socket binded.\n");
 	}
 
-
 	err = listen(server_socket,10);
 	if(err){
 		printf("listen err: %i\n",err);
@@ -34,10 +33,22 @@ int main(){
 		printf("socket listening.\n");
 	}
 
-	sock = accept(server_socket,NULL,NULL);
+	return server_socket;
 
-	printf("server_socket:%i \n",server_socket);
-	printf("sock:%i \n",server_socket);
-	return 0;
 }
 
+/* start a new thread for each new player */
+void handle(int player_fd)
+{
+	printf("TODO handle %i \n",player_fd);
+}
+
+int main()
+{
+	int np = 0;
+	int server_socket=init_server();
+	while(1){
+		np = accept(server_socket,NULL,NULL);
+		handle(np);
+	}
+}
