6cca3563db516445a621de40045899b149952c2f
r0nk <r00nk@simplecpu.com>
2015-09-15 16:21:37 +0000

    added server makefile and main hello world

--- common/protolol.h
+++ common/protolol.h
@@ -7,6 +7,7 @@ struct protolol_packet {
 	char magic_start[4];// should be "oHai"
 	char type;// whether or not its a game state or kick or whatever
 	int length;//in bytes, this header included.
+	char data[500];
 }
  
 #endif
