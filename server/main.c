07e978c8c8bf9d64cb2d410a507191cbe0f7dd33
r0nk <r00nk@simplecpu.com>
2015-09-15 17:05:59 +0000

    started on server networking

--- server/main.c
+++ server/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 int main(){
-	printf("hello world!\n");
+	struct sockaddr address;
+	printf("hello world! \n");
+	int s = socket(PF_INET,SOCK_STREAM,0);
+
+	int err = 0;
+	err = listen(s,10);
+	if(err){
+		printf("listen err \n");
+		exit(-1);
+	}
+
+	//TODO: bind and accept
+
+	printf("s:%i \n",s);
+	return 0;
 }
+
