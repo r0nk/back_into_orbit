e5b72b31ac87958533c9b8a6858e4163a55a3f52
r0nk <r00nk@simplecpu.com>
2015-11-28 21:28:18 -0700

    added more framework for server

--- overlord/main.c
+++ overlord/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+void handle_player_login()
+{
+	//TODO: make the magic happen in here.
+	//figure out the right server for the player
+	//add_player_to_servling(p,right_server);
+}
+
+void transfer_player()
+{
+	//TODO: make the magic happen in here.
+	// tell s1(players current server) to drop player
+	//add_player_to_servling(p,s2);
+}
+
+/*
+void add_player_to_servling(struct player * p, struct servling * s)
+{
+	// tell servling its about to get a player p
+	// tell the player to connect to the servling s
+}
+*/
+
 int main()
 {
 	printf("hello world!\n");
