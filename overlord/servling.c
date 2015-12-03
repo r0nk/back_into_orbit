52bcc2e6f5b021a35b6c6b22cd26d1cd187f262d
r0nk <r00nk@simplecpu.com>
2015-12-02 20:26:41 -0700

    added servling packet handler prototype

--- overlord/servling.c
+++ overlord/servling.c
@@ -1,5 +1,19 @@
 #include "servling.h"
 
+void handle_servling_packet(int fd)
+{
+	struct protolol_packet pp;
+	pp = recv_protolol(fd);
+	switch(pp.type){
+		case PROTOLOL_TYPE_CLIENT_TELEPORT:
+			client_connect_to(np,"127.0.0.1");
+			break;
+		default:
+			err(-20,"overlord recived unknown packet type");
+			break;
+	}
+}
+
 void servling_handler(void * servling)
 {
 	struct servling s = (struct servling) servling;
