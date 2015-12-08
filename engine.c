39078f901ecdce71babbe7a09d2fda485b7df1a9
r0nk <r00nk@simplecpu.com>
2015-12-08 01:47:57 -0700

    took firing back from old setup

--- engine.c
+++ engine.c
@@ -15,6 +15,35 @@ double delta_time()
 	return s;
 }
 
+void update_bullets(struct game_state * gs, double delta)
+{
+	int i;
+	double s;
+	for(i=0;i<gs->n_bullets;i++){
+		s = gs->bullet[i].speed*delta;
+		gs->bullet[i].location.x+=
+			gs->bullet[i].direction.x * s;
+		gs->bullet[i].location.z+=
+			gs->bullet[i].direction.z * s;
+		if(world_room.tiles[(int)gs->bullet[i].location.x]
+				[(int)gs->bullet[i].location.z])
+			remove_bullet(gs,i);
+	}
+}
+
+void fire_bullet(struct game_state * gs,
+		struct vector starting, struct vector direction)
+{
+	struct bullet b;
+	b.location.x = starting.x+direction.x;
+	b.location.y = starting.y+direction.y;
+	b.location.z = starting.z+direction.z;
+	b.direction = direction;
+	b.speed = 5;
+	b.duration = 100;
+	add_bullet(gs,b);
+}
+
 void move_unit(struct unit * u,struct vector d)
 {
 	if(world_room.tiles[ (int)(u->location.x+d.x) ][(int)(u->location.z)]
@@ -50,9 +79,45 @@ void player_movement(struct game_state * gs, double delta)
 	move_unit(&gs->game_player,dvec);
 }
 
+void player_attack(struct game_state * gs, double delta)
+{
+#define FR 4
+	if(gs->game_player.cooldown<1){
+		if(pi.keys['J']){
+			fire_bullet(gs,gs->game_player.location,
+					(struct vector) {1,0,1});
+			gs->game_player.cooldown = FR;
+		}
+		if(pi.keys['H']){
+			fire_bullet(gs,gs->game_player.location,
+					(struct vector) {-1,0,1});
+			gs->game_player.cooldown = FR;
+		}
+		if(pi.keys['K']){
+			fire_bullet(gs,gs->game_player.location,
+					(struct vector) {-1,0,-1});
+			gs->game_player.cooldown = FR;
+		}
+		if(pi.keys['L']){
+			fire_bullet(gs,gs->game_player.location,
+					(struct vector) {1,0,-1});
+			gs->game_player.cooldown = FR;
+		}
+	} else {
+		gs->game_player.cooldown-=10*delta;
+	}
+}
+
+void update_player(struct game_state * gs,double delta)
+{
+	player_movement(gs,delta);
+	player_attack(gs,delta);
+}
+
 /* all of the game engine stuff is actually server side */
 void engine_tick(struct game_state * gs)
 {
 	double d = delta_time();
-	player_movement(gs,d);
+	update_player(gs,d);
+	update_bullets(gs,d);
 }
