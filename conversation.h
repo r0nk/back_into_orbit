a26f79d54c7112f0798a0072967c2c7e45f63273
r0nk <r00nk@simplecpu.com>
2015-12-26 00:25:39 -0700

    conversation debug stuff

--- conversation.h
+++ conversation.h
@@ -7,10 +7,29 @@ struct line {
 	char words[80];/* the actually words the unit is going to say */
 };
 
+void dump_line(struct line line)
+{
+	printf("line speaker: %c\n",line.speaker);
+	printf("line duration: %i\n",line.duration);
+	printf("\"%s\"\n",line.words);
+}
+
 struct conversation
 {
 	struct unit * a,b;
-	char lines[24][80];
+	int n_lines;
+	struct line line[24];
 };
+
+void dump_conversation(struct conversation convo)
+{
+	printf("conversation between %p and %p",convo.a,convo.b);
+	printf("n_lines:%i",convo.n_lines);
+	int i;
+	for(i=0;i<10;i++){
+		printf("line[%i]:\n",i);
+		dump_line(line[i]);
+	}
+}
  
 #endif
